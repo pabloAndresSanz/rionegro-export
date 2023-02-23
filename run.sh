88bdd6347007c6a724443724c2ed081724f24d43
Pablo Sanz <pablosanz@gmail.com>
2023-02-23 06:53:06 -0300

    run.sh para los que estén en linux o mac

---
+++ run.sh
@@ -0,0 +1,9 @@
+export SINIDE_DB_HOST=pg-staging.die.educacion.gob.ar
+export SINIDE_DB_PORT=5432
+export SINIDE_DB_NAME=sinideprod
+export SINIDE_DB_USERNAME=redfie
+export SINIDE_DB_PASSWORD=redfie11
+export SINIDE_DB_ESQUEMA=jujuy
+
+java -cp . -jar target/sinide-export-0.0.1-SNAPSHOT.jar
+
