863eab84312a1af5b0f37301570a842630b4c643
Pablo Sanz <pablosanz@gmail.com>
2023-03-07 07:33:27 -0300

    run sin usuario y passwords

--- run.bat
+++ run.bat
@@ -2,8 +2,8 @@
 set SINIDE_DB_HOST=pg-staging.die.educacion.gob.ar
 set SINIDE_DB_PORT=5432
 set SINIDE_DB_NAME=sinideprod
-set SINIDE_DB_USERNAME=redfie
-set SINIDE_DB_PASSWORD=redfie11
+set SINIDE_DB_USERNAME=
+set SINIDE_DB_PASSWORD=
 set SINIDE_DB_ESQUEMA=jujuy
 
 java -cp . -jar target\sinide-export-0.0.1-SNAPSHOT.jar
